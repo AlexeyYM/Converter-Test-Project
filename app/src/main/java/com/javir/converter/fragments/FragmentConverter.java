f8d004d400a57d14a12dabaeb06e59f39707fb43
AlexeyYM <a.yuran@list.ru>
2017-03-06 10:44:15 +0300

    Filled fragments and implemented the main functionality of the application.

--- app/src/main/java/com/javir/converter/fragments/FragmentConverter.java
+++ app/src/main/java/com/javir/converter/fragments/FragmentConverter.java
@@ -6,6 +6,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.javir.converter.R;
 
@@ -14,6 +18,9 @@ import com.javir.converter.R;
  */
 public class FragmentConverter extends AbstractTabFragment {
     private static final int LAYOUT = R.layout.layout_fragment_converter;
+
+    private Button convOldButton;
+    private Button convNewButton;
 
     public static FragmentConverter getInstance(Context context) {
         Bundle args = new Bundle();
@@ -30,7 +37,62 @@ public class FragmentConverter extends AbstractTabFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT, container, false);
 
+        convOldButton = ((Button) view.findViewById(R.id.convOldButton));
+        convNewButton = ((Button) view.findViewById(R.id.convNewButton));
+
+        convOldButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                convOld();
+            }
+        });
+
+        convNewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                convNew();
+            }
+        });
+
         return view;
+    }
+
+    public void convOld() {
+        EditText inputOldSum = (EditText) view.findViewById(R.id.inputConvSumOld);
+        TextView outputOldSum = (TextView) view.findViewById(R.id.convOutputSum);
+
+        if(inputOldSum.getText().length() == 0) {
+            Toast.makeText(context, "Введите сумму", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        double inputValue = Double.parseDouble(inputOldSum.getText().toString());
+        double outputValue = convertOld(inputValue);
+
+        outputOldSum.setText(String.format("$%,.2f", outputValue));
+    }
+
+    public double convertOld(double d) {
+        return d / 20000;
+    }
+
+    public void convNew() {
+        EditText inputOldSum = (EditText) view.findViewById(R.id.inputConvSumNew);
+        TextView outputOldSum = (TextView) view.findViewById(R.id.convOutputSum);
+
+        if(inputOldSum.getText().length() == 0) {
+            Toast.makeText(context, "Введите сумму", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        double inputValue = Double.parseDouble(inputOldSum.getText().toString());
+        double outputValue = convertNew(inputValue);
+
+        outputOldSum.setText(String.format("$%,.2f", outputValue));
+    }
+
+    public double convertNew(double d) {
+        return d / 2;
     }
 
     public void setContext(Context context) {
--- app/src/main/java/com/javir/converter/fragments/FragmentDenomination.java
+++ app/src/main/java/com/javir/converter/fragments/FragmentDenomination.java
@@ -6,6 +6,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.javir.converter.R;
 
@@ -14,6 +18,9 @@ import com.javir.converter.R;
  */
 public class FragmentDenomination extends AbstractTabFragment {
     private static final int LAYOUT = R.layout.layout_fragment_denomination;
+
+    private Button denominationOldButton;
+    private Button denominationNewButton;
 
     public static FragmentDenomination getInstance(Context context) {
         Bundle args = new Bundle();
@@ -30,7 +37,62 @@ public class FragmentDenomination extends AbstractTabFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT, container, false);
 
+        denominationOldButton = (Button) view.findViewById(R.id.denominationOldButton);
+        denominationNewButton = (Button) view.findViewById(R.id.denominationNewButton);
+
+        denominationOldButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                denominationOldButton();
+            }
+        });
+
+        denominationNewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                denominationNewButton();
+            }
+        });
+
         return view;
+    }
+
+    public void denominationOldButton() {
+        EditText inputOldSum = (EditText) view.findViewById(R.id.inputOld);
+        TextView outputOldSum = (TextView) view.findViewById(R.id.outputOldText);
+
+        if(inputOldSum.getText().length() == 0) {
+            Toast.makeText(context, "Введите сумму", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        double inputValue = Double.parseDouble(inputOldSum.getText().toString());
+        double outputValue = convertOld(inputValue);
+
+        outputOldSum.setText(String.format("%,.2f рублей", outputValue));
+    }
+
+    public void denominationNewButton() {
+        EditText inputNewSum = (EditText) view.findViewById(R.id.inputNew);
+        TextView outputNewSum = (TextView) view.findViewById(R.id.outputNewText);
+
+        if(inputNewSum.getText().length() == 0) {
+            Toast.makeText(context, "Введите сумму", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        double inputValue = Double.parseDouble(inputNewSum.getText().toString());
+        double outputValue = convertNew(inputValue);
+
+        outputNewSum.setText(String.format("%,.0f рублей", outputValue));
+    }
+
+    public double convertOld(double x) {
+        return x / 10000;
+    }
+
+    public double convertNew(double x) {
+        return x * 10000;
     }
 
     public void setContext(Context context) {
